9e5c26eea497624fad43977f2e27082499ef5df0
Tomasz Buchta <tomasz.buchta@hotmail.com>
2023-01-24 21:00:14 +0100

    Clarify what the Loader class is for

--- lib/ad_parameters/loader.rb
+++ lib/ad_parameters/loader.rb
@@ -4,8 +4,10 @@ module FYBER
 end
 
 module AdParameters
+  # Class responsible for loading the code to avoid adding requires everywhere
   class Loader
     def self.run
+      # Zeitwerk is a ruby code loader used by rails
       loader = Zeitwerk::Loader.new # Code loader to avoid all the require statements
       loader.push_dir("lib/ad_parameters", namespace: AdParameters)
       loader.push_dir("lib/fyber", namespace: FYBER)
