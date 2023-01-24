4290b5d3427cab725bfe530f02c8b1995f7d2765
Tomasz Buchta <tomasz.buchta@hotmail.com>
2023-01-24 20:36:12 +0100

    Serialize and print protobuf

--- lib/ad_parameters.rb
+++ lib/ad_parameters.rb
@@ -25,9 +25,15 @@ def main
   creatives = AdParameters::DataFactory.new(AdParameters::Entities::Creative, creative_nodes).build
   placements = AdParameters::DataFactory.new(AdParameters::Entities::Placement, placement_nodes).build
 
+  # Assign creatives to placements
   mapped = placements.map{ |placement| [placement, AdParameters::MatchCreativesToPlacement.new(creatives:, placement:).call] }.to_h
-
-  require 'pry'; binding.pry
+  # And to serialize them
+  seq = mapped.map do |placement, creatives|
+    creatives_protobufs = creatives.map{ |c| FYBER::Userconfiguration::Creative.new(id: c.id, price: c.price.to_f) }
+    FYBER::Userconfiguration::Placement.new(id: placement.id, creative: creatives_protobufs)
+  end
+  placement_seq = FYBER::Userconfiguration::PlacementSeq.new(placement: seq)
+  puts placement_seq.to_json
 end
 
 # Run the main
